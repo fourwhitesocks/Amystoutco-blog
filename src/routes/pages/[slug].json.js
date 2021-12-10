90c92408b9178ca6c2913c1a2438274140011f72
fourwhitesocks <asbilly92@gmail.com>
2021-12-10 09:53:14 -0500

    fix: :bug: Fixed the pages slug

--- src/routes/pages/[slug].json.js
+++ src/routes/pages/[slug].json.js
@@ -2,39 +2,30 @@ import {client} from '$lib/graphql-client'
 import { gql } from 'graphql-request'
 
 //is now doing a get request for any page which matches the slug passed in the qurey
-export const get = async (req) => {
+export const get = async req => {
     const slug = req.params.slug
-
     try {
-        const query = gql`
-        query Page ($slug: string!) {
-            page(where: {slug: $slug}) {
-              title
-              content {
-                html
-              }
+      const query = gql`
+        query Page($slug: String!) {
+          page(where: { slug: $slug }) {
+            title
+            content {
+              html
             }
           }
-          `
-
-          const variables = { slug }
-          
-          const { page } = await client.request(query, variables)
-
-          return {
-              status: 200,
-              body: { page }
-          }
-
-
-    } catch (error) {
-
-        return{
-            status:500,
-            body: {error: 'There was a server error.'}
         }
-
+      `
+      const variables = { slug }
+      const { page } = await client.request(query, variables)
+  
+      return {
+        status: 200,
+        body: { page },
+      }
+    } catch (error) {
+      return {
+        status: 500,
+        body: { error: 'There was a server error.' },
+      }
     }
-
-
-}
+  }
