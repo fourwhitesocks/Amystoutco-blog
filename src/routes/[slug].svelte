caf60a6c2b38099738fd0e2c6ba47d341782058f
amy stout <asbilly92@gmail.com>
2022-01-08 17:35:23 -0500

    fix: :bug: changed the [slug] async function and fixed the load error `page` in `load` functions has been replaced by `url` and `params`

--- src/routes/[slug].svelte
+++ src/routes/[slug].svelte
@@ -1,33 +1,27 @@
 <script context="module">
-
-    export const load = async ({ fetch, page: { params } }) => {
-        const { slug } = params
-      //res is response and it will await the response and then return the props which are the posts
-      const res = await fetch(`/pages/${slug}.json`)
-      if (res.ok) {
-        const { page } = await res.json()
-        return { 
-          props: { page } ,
-        }
+  export async function load({ params, fetch }) {
+    const { slug } = params
+    //res is response and it will await the response and then return the props which are the page
+    const res = await fetch(`/pages/${slug}.json`)
+    if (res.ok) {
+      const { page } = await res.json()
+      return {
+        props: { page },
       }
     }
-  
-  </script>
-  
-  
-  <script>
-        export let page
+  }
+</script>
 
-        
-  </script>
-  
-  
-  <svelte:head>
-    <title>Amy Blog</title>
-  </svelte:head>
+<script>
+  export let page
+</script>
 
-  <h1 class="text-xl font-semibold mb-5">{ page.title }</h1>
+<svelte:head>
+  <title>Amy Blog</title>
+</svelte:head>
+
+<h1 class="text-xl font-semibold mb-5">{page.title}</h1>
 
 <article class="prose">
-    { @html page.content.html }
+  {@html page.content.html}
 </article>
